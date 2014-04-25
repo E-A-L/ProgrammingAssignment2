b19c61dd1c3c96a0220dc4fbffe42c90ac16694e
Eric <lonvick@ameritech.net>
2014-04-24 23:36:53 -0500

    Update the logic with comments
    Added comments to both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,21 @@
 ## E. Lonvick; programming Assignment 2
 ## This funcation stores a copy of the object in memory
 ## The second time it is reference without changes; it will return the value(s)
-##  This is an abstract object with accessor and get function.
+##  This is an abstract object with accessor (set) and get functions.
 ##
 ##
 makeCacheMatrix <- function(x = matrix()) {
+#       CacheMatrix is a holder in memory
         CacheMatrix <- NULL
-
+#        If the value is not null the update memory otherwise save it for later use.
         set <- function(y) {
                 x <<- y
                 CacheMatrix <<- NULL
         }
         get <- function() x
+        # setmatrix will store the 'matrix' in memory for the next function call.
         setMatrix <- function(CacheMatrix)  CacheMatrix <<- x 
+        # getmatrict will retrieve teh value if is not null; no need to recommpute if not null.
         getMatrix <- function()  CacheMatrix
         list(set = set, get = get,
              setMatrix = setMatrix,
@@ -29,6 +32,7 @@ cacheSolve <- function(x, ...) {
                 message("getting cached data")
                 return(m)
         }
+        # get obejct from memory and compute the values if different
         data <- x$get()
         m <- solve(data, ...)
         x$setMatrix(m)
