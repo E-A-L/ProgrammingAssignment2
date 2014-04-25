315a030cb9c37e1ac7071e833d7e7a1c76671032
Eric <lonvick@ameritech.net>
2014-04-24 23:17:58 -0500

    Update cachematrix.R
    The revision has the logic and comments included

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## E. Lonvick; programming Assignment 2
+## This funcation stores a copy of the object in memory
+## The second time it is reference without changes; it will return the value(s)
+##  This is an abstract object with accessor and get function.
+##
+##
 makeCacheMatrix <- function(x = matrix()) {
+        CacheMatrix <- NULL
+
+        set <- function(y) {
+                x <<- y
+                CacheMatrix <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(CacheMatrix)  CacheMatrix <<- x 
+        getMatrix <- function()  CacheMatrix
+        list(set = set, get = get,
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)
 
 }
-
-
-## Write a short comment describing this function
-
+##
+## This fuction will access the object help in memory
+## and return the inverse matrix and no processing is needed
+## becuase it was cached in memory.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getMatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setMatrix(m)
+        m
 }
